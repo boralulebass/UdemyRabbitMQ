056be6554b554945c8106518bc9ebbb5b12f5f32
boralulebass <boralulebass@gmail.com>
2023-07-06 01:23:47 +0300

    Add project files.

---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Xml.Schema;
+
+namespace UdemyRabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://jwhtwypr:j6FED0tFdY5hSMni99ndYkNNwudznKmU@woodpecker.rmq.cloudamqp.com/jwhtwypr");
+
+            using var connection = factory.CreateConnection();
+            
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"log {x}";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("logs-fanout","", null, messageBody);
+
+                Console.WriteLine($"Mesaj Gönderilmiştir. {message}");
+            });
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQWeb.Watermark/BackgroundServices/ImageWatermarkProccessBackgroundService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using UdemyRabbitMQWeb.Watermark.Services;
+
+namespace UdemyRabbitMQWeb.Watermark.BackgroundServices
+{
+    public class ImageWatermarkProccessBackgroundService : BackgroundService
+    {
+        private readonly RabbitMQClientService _rabbitmqClientService;
+        private readonly ILogger<ImageWatermarkProccessBackgroundService> _logger;
+        private RabbitMQ.Client.IModel _channel;
+        public ImageWatermarkProccessBackgroundService(RabbitMQClientService rabbitmqClientService, ILogger<ImageWatermarkProccessBackgroundService> logger)
+        {
+            _rabbitmqClientService = rabbitmqClientService;
+            _logger = logger;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _channel = _rabbitmqClientService.Connect();
+            _channel.BasicQos(0, 1, false);
+            return base.StartAsync(cancellationToken);
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+            _channel.BasicConsume(RabbitMQClientService.QueueName,false, consumer);
+            consumer.Received += Consumer_Received;
+
+            return Task.CompletedTask;
+
+        }
+
+        private Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            try 
+            {
+                var productImageCreatedEvent = JsonSerializer.Deserialize<ProductImageCreatedEvent>(Encoding.UTF8.GetString(@event.Body.ToArray()));
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", productImageCreatedEvent.ImageName);
+
+                using var img = Image.FromFile(path);
+
+                var siteName = "www.mysite.com";
+
+                using var graphic = Graphics.FromImage(img);
+
+                var font = new Font(FontFamily.GenericMonospace, 32, FontStyle.Bold, GraphicsUnit.Pixel);
+
+                var textSize = graphic.MeasureString(siteName, font);
+
+                var color = Color.FromArgb(128, 255, 255, 255);
+                var brush = new SolidBrush(color);
+
+                var position = new Point(img.Width - ((int)textSize.Width + 30), img.Height - ((int)textSize.Height + 30));
+
+                graphic.DrawString(siteName, font, brush, position);
+                img.Save("wwwroot/images/watermarks/" + productImageCreatedEvent.ImageName);
+
+                img.Dispose();
+                graphic.Dispose();
+                _channel.BasicAck(@event.DeliveryTag, false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+
+            return Task.CompletedTask;
+
+        }
+    }
+}
